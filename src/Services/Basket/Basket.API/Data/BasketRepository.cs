3fcd9879ccdfea5605b227de15f4081f9acb86bc
kurochkinvr <pin11@kurochkin@gmail.com>
2024-06-02 23:51:49 +0300

    #feature/basket-1 *Репозиторий корзины

---
+++ src/Services/Basket/Basket.API/Data/BasketRepository.cs
@@ -0,0 +1,34 @@
+﻿using Basket.API.Exceptions;
+using Basket.API.Models;
+using Marten;
+
+namespace Basket.API.Data;
+
+public class BasketRepository : IBasketRepository
+{
+    private readonly IDocumentSession _session;
+
+    public BasketRepository(IDocumentSession session) 
+        => _session = session;
+
+    public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken)
+    {
+        var basket = await _session.LoadAsync<ShoppingCart>(userName, cancellationToken);
+
+        return basket ?? throw new BasketNotFoundException(userName);
+    }
+
+    public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken)
+    {
+        _session.Store(basket);
+        await _session.SaveChangesAsync(cancellationToken);
+        return basket;
+    }
+
+    public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken)
+    {
+        _session.Delete<ShoppingCart>(userName);
+        await _session.SaveChangesAsync(cancellationToken);
+        return true;
+    }
+}
