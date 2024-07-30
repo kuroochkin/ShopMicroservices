8db4d803cab035109556f08b40e83b6ccc246cfe
kuroochkin <pin11kurochkin@gmail.com>
2024-07-30 22:08:47 +0300

    #feature/grpc
    *Настройка базового GRPC + SQLite

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,3 +1,8 @@
+using Discount.Grpc.Data;
+using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
+using Discount.Grpc.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Additional configuration is required to successfully run gRPC on macOS.
@@ -6,10 +11,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddGrpc();
 
+builder.Services.AddDbContext<DiscountContext>(opts =>
+    opts.UseSqlite(builder.Configuration.GetConnectionString("Database")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-// app.MapGrpcService<GreeterService>();
+// migrations use
+app.MapGrpcService<DiscountService>();
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
