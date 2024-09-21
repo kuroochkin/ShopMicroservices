e916ee050dde6396eef1d1c521458e4448396d98
kuroochkin <pin11kurochkin@gmail.com>
2024-09-21 19:00:06 +0300

    #feature/ordering *Валидация + Exceptions

---
+++ src/Services/Ordering/Ordering.Domain/Exceptions/ArgumentValidation.cs
@@ -0,0 +1,22 @@
+﻿namespace Ordering.Domain.Exceptions;
+
+public static class ArgumentValidation
+{
+    public static void ThrowIfNegativeOrZero(int value, string paramName)
+    {
+        if (value <= 0)
+            throw new ArgumentOutOfRangeException(paramName, "Value must be greater than zero.");
+    }
+
+    public static void ThrowIfNegativeOrZero(decimal value, string paramName)
+    {
+        if (value <= 0)
+            throw new ArgumentOutOfRangeException(paramName, "Value must be greater than zero.");
+    }
+    
+    public static void ThrowIfGreaterThan(int value, int maxValue, string paramName)
+    {
+        if (value > maxValue)
+            throw new ArgumentOutOfRangeException(paramName, $"Value {value} cannot be greater than {maxValue}.");
+    }
+}
