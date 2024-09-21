f7b2cdab7e1dd79f8eb18ede13a5ae9cc1460842
kuroochkin <pin11kurochkin@gmail.com>
2024-09-21 19:00:31 +0300

    #feature/ordering *Работа с сущностями

--- src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -1,20 +1,22 @@
 ﻿using Ordering.Domain.Abstractions;
+using Ordering.Domain.ValueObjects;
 
 namespace Ordering.Domain.Models;
 
-public class OrderItem : Entity<Guid>
+public class OrderItem : Entity<OrderItemId>
 {
-    public Guid OrderId { get; private set; } = default!;
-    public Guid ProductId { get; private set; } = default!;
+    public OrderId OrderId { get; private set; } = default!;
+    public ProductId ProductId { get; private set; } = default!;
     public int Quantity { get; private set; } = default!;
     public decimal Price { get; private set; } = default!;
     
     public OrderItem(
-        Guid orderId, 
-        Guid productId, 
+        OrderId orderId, 
+        ProductId productId, 
         int quantity, 
         decimal price)
     {
+        Id = OrderItemId.Of(Guid.NewGuid());
         OrderId = orderId;
         ProductId = productId;
         Quantity = quantity;
--- src/Services/Ordering/Ordering.Domain/Models/Product.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -1,10 +1,28 @@
 ﻿using Ordering.Domain.Abstractions;
+using Ordering.Domain.Enums;
+using Ordering.Domain.Exceptions;
+using Ordering.Domain.ValueObjects;
 
 namespace Ordering.Domain.Models;
 
-public class Product : Entity<Guid>
+public class Product : Entity<ProductId>
 {
     public string Name { get; private set; } = default!;
 
     public decimal Price { get; private set; } = default!;
+    
+    public static Product Create(ProductId id, string name, decimal price)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+        ArgumentValidation.ThrowIfNegativeOrZero(price, nameof(price));
+        
+        var product = new Product
+        {
+            Id = id,
+            Name = name,
+            Price = price
+        };
+
+        return product;
+    }
 }
