423151d0d347ecc4ff16f08c94c1c5e135edcb3f
kuroochkin <pin11kurochkin@gmail.com>
2024-10-18 17:32:07 +0300

    #feature/appOrder *Удаление заказа

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderHandler.cs
@@ -0,0 +1,31 @@
+﻿using BuildingBlocks.CQRS;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Data;
+using Ordering.Application.Exceptions;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Application.Orders.Commands.DeleteOrder;
+
+public class DeleteOrderHandler
+    : ICommandHandler<DeleteOrderCommand, DeleteOrderResult>
+{
+    private readonly IApplicationDbContext _dbContext;
+
+    public DeleteOrderHandler(IApplicationDbContext dbContext) 
+        => _dbContext = dbContext;
+    
+    public async Task<DeleteOrderResult> Handle(DeleteOrderCommand command, CancellationToken cancellationToken)
+    {
+        var orderId = OrderId.Of(command.OrderId);
+        var order = await _dbContext.Orders
+            .SingleOrDefaultAsync(a => a.Id == orderId, cancellationToken);
+
+        if (order is null)
+            throw new OrderNotFoundException(command.OrderId);
+        
+        _dbContext.Orders.Remove(order);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+
+        return new DeleteOrderResult(true);        
+    }
+}
