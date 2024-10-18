ca058db725234f8a93ce82332244fad03cc54181
kuroochkin <pin11kurochkin@gmail.com>
2024-10-18 17:56:20 +0300

    #featute/appOrder *Получение заказа по заказчику

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerHandler.cs
@@ -0,0 +1,32 @@
+﻿using BuildingBlocks.CQRS;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Data;
+using Ordering.Application.Extensions;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer;
+
+public class GetOrdersByCustomerHandler
+    : IQueryHandler<GetOrdersByCustomerQuery, GetOrdersByCustomerResult>
+{
+    private readonly IApplicationDbContext _dbContext;
+
+    public GetOrdersByCustomerHandler(IApplicationDbContext dbContext) 
+        => _dbContext = dbContext;
+    
+    public async Task<GetOrdersByCustomerResult> Handle(
+        GetOrdersByCustomerQuery query, CancellationToken cancellationToken)
+    {
+        // get orders by customer using dbContext
+        // return result
+
+        var orders = await _dbContext.Orders
+            .Include(o => o.OrderItems)
+            .AsNoTracking()
+            .Where(o => o.CustomerId == CustomerId.Of(query.CustomerId))
+            .OrderBy(o => o.OrderName.Value)
+            .ToListAsync(cancellationToken);
+
+        return new GetOrdersByCustomerResult(orders.ToOrderDtoList());        
+    }
+}
