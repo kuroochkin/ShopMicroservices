7a659dc4e4a9a18c4bbf5e6b550690054063270b
kuroochkin <pin11kurochkin@gmail.com>
2024-10-25 00:26:20 +0300

    #feature/rabbitMQ *Добавление FeatureManagement

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -0,0 +1,37 @@
+﻿using MassTransit;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Microsoft.FeatureManagement;
+using Ordering.Application.Extensions;
+using Ordering.Domain.Events;
+
+namespace Ordering.Application.Orders.EventHandlers.Domain;
+
+public class OrderCreatedEventHandler
+    : INotificationHandler<OrderCreatedEvent>
+{
+    private readonly ILogger<OrderCreatedEventHandler> _logger;
+    private readonly IPublishEndpoint _publishEndpoint;
+    private readonly IFeatureManager _featureManager;
+
+    public OrderCreatedEventHandler(
+        ILogger<OrderCreatedEventHandler> logger,
+        IPublishEndpoint publishEndpoint,
+        IFeatureManager featureManager)
+    {
+        _logger = logger;
+        _publishEndpoint = publishEndpoint;
+        _featureManager = featureManager;
+    }
+    
+    public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
+        
+        if (await _featureManager.IsEnabledAsync("OrderFullfilment"))
+        {
+            var orderCreatedIntegrationEvent = domainEvent.Order.ToOrderDto();
+            await _publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+        }
+    }
+}
